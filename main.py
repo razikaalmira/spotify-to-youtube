adbf01090942b8f68c9387b49af61ba8bc7c72ec
Razika <rararafa4@gmail.com>
2024-06-13 22:01:28 +0700

    Forgot to remove credentials lol

--- main.py
+++ main.py
@@ -1,83 +1,82 @@
+import os
+import time
+from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials # to fetch unrelated data of a user
 from spotipy.oauth2 import SpotifyOAuth
 from ytmusicapi import YTMusic
-import os
-import time
 
 # setting up the credentials
-os.environ['SPOTIPY_CLIENT_ID'] = 'b5734e600d6d431c8a3c78cb170c599c'
-os.environ['SPOTIPY_CLIENT_SECRET'] = '14579bc988444acc9f396bce4f9335c8'
-os.environ['SPOTIPY_REDIRECT_URI'] = 'http://localhost:8181/callback'
+load_dotenv()
 
+class SpotifyToYoutube:
+    def __init__(self,oauth_json: str):
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope='playlist-read-private'))
+        self.oauth_json = oauth_json
+        self.yt = YTMusic(oauth_json)
 
-def get_spo_playlists() -> list:
-    """
-    Fetch saved playlists by the user
+    def get_spo_playlists(self) -> list:
+        """
+        Fetch saved playlists by the user
 
-    Returns:
-        An array of tuples containing playlist id, playlist name, and playlist owner.
-    """
-    
-    scope = "playlist-read-private"
-    
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    results2 = sp.current_user_playlists()
-    
-    ids = [item['id'] for item in results2['items']]
-    names = [item['name'] for item in results2['items']]
-    owners = [item['owner']['display_name'] for item in results2['items']]
-    
-    playlist_ids = list(zip(ids,names,owners))
-    return playlist_ids
+        Returns:
+            An array of tuples containing playlist id, playlist name, and playlist owner.
+        """
+        results = self.sp.current_user_playlists()
+        
+        ids = [item['id'] for item in results['items']]
+        names = [item['name'] for item in results['items']]
+        owners = [item['owner']['display_name'] for item in results['items']]
+        
+        playlist_ids = list(zip(ids,names,owners))
+        return playlist_ids
 
-def get_spo_songs(playlist_id: str) -> list:
-    """
-    Fetch songs that contain in a playlist
-    
-    Args:
-        playlist_id: a Spotify playlist ID
+    def get_spo_songs(self,playlist_id: str) -> list:
+        """
+        Fetch songs that contain in a playlist
+        
+        Args:
+            playlist_id: a Spotify playlist ID
 
-    Returns:
-        an array of the artist name and song title
-    """
-    
-    scope = "playlist-read-private"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    
-    results2 = sp.playlist_items(playlist_id=playlist_id)
+        Returns:
+            an array of the artist name and song title
+        """
 
-    song_list = []
-    for i in range(len(results2['items'])):
-        song_name = results2['items'][i]['track']['name']
-        song_artist = results2['items'][i]['track']['artists'][0]['name']
-        song_list.append(song_artist+' '+song_name)
-    return song_list
+        results = self.sp.playlist_items(playlist_id=playlist_id)
 
-def from_spo_to_yt(oauth_json: str) -> None:
-    """
-    Move Spotify playlists to Youtube
-    
-    Args:
-        oauth_json: name of a json file that contain Youtube API credentials
-    """
-    yt = YTMusic(oauth_json)
-    yt_playlists = yt.get_library_playlists()
-    yt_playlist_names = set([yt_playlists[i]['title'] for i in range(len(yt_playlists))])
+        song_list = []
+        for i in range(len(results['items'])):
+            song_name = results['items'][i]['track']['name']
+            song_artist = results['items'][i]['track']['artists'][0]['name']
+            song_list.append(song_artist+' '+song_name)
+        return song_list
 
-    playlists = get_spo_playlists()
+    def from_spo_to_yt(self) -> None:
+        """
+        Move Spotify playlists to Youtube
+        
+        Args:
+            oauth_json: name of a json file that contain Youtube API credentials
+        """
+        yt_playlists = self.yt.get_library_playlists()
+        yt_playlist_names = set([yt_playlists[i]['title'] for i in range(len(yt_playlists))])
 
-    for playlistid,name,owner in playlists:
-        if name not in yt_playlist_names:
-            playlist_yt = yt.create_playlist(title=name,description=name+' created by '+owner+' from Spotify')
-            songlist = get_spo_songs(playlistid)
-            for song in songlist:
-                search_results = yt.search(song)
-                yt.add_playlist_items(playlist_yt,[search_results[0]['videoId']])
-                print(f'Succesfully added {song} to {name} playlist')
+        playlists = self.get_spo_playlists()
+
+        for playlistid,name,owner in playlists:
+            if name not in yt_playlist_names:
+                playlist_yt = self.yt.create_playlist(title=name,description=name+' created by '+owner+' from Spotify')
+                songlist = self.get_spo_songs(playlistid)
+                for song in songlist:
+                    search_results = self.yt.search(song)
+                    if search_results:
+                        self.yt.add_playlist_items(playlist_yt,[search_results[0]['videoId']])
+                        print(f'Succesfully added {song} to {name} playlist')
+                        time.sleep(5)
                 time.sleep(30)
-        else:
-            print(f'Playlist {name} is already available in your Youtube account')
+            else:
+                print(f'Playlist {name} is already available in your Youtube account')
 
 if __name__ == "__main__":
-    from_spo_to_yt('oauth.json')
+    spotify_to_youtube = SpotifyToYoutube('oauth.json')
+    spotify_to_youtube.from_spo_to_yt()
